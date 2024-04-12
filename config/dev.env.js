53f377e3849e5a2ec48544f2b38c68494c529fea
danny <danieljoquinn@gmail.com>
2024-04-12 00:05:54 -0400

    Updates

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-var meta = require('./meta.json')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  META: JSON.stringify(meta)
-})
